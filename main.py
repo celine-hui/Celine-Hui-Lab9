0187cd28ed7f9eedc5abf07831f0ef1ffbe63c86
celine-hui <166661432+celine-hui@users.noreply.github.com>
2024-04-11 14:37:28 -0400

    updated decode function commit

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def encode(password):
             encoded += str(num + 3)
     return encoded
 
+# updated decode function
+def decode(password):
+    decoded = ""
+    for num in password:
+        num = int(num)
+        if num <= 2:
+            decoded += str(num + 7)
+        else:
+            decoded += str(num - 3)
+    return decoded
+
 while True:
     print()
     print("Menu")
@@ -25,10 +36,9 @@ while True:
     if option == 1:
         password = input("Please enter your password to encode: ")
         encoded_password = encode(password)
-        print(encoded_password)
         print("Your password has been encoded and stored! ")
     elif option == 2:
         decoded_password = decode(encoded_password)
-        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}")
+        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
     elif option == 3:
         break
