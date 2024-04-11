1f651fe69d772453eb58f6d0c049cfc5b0d7f3ef
celine-hui <166661432+celine-hui@users.noreply.github.com>
2024-04-11 14:31:45 -0400

    Celine Hui - encode function commit

---
+++ main.py
@@ -0,0 +1,34 @@
+
+# Celine Hui - encode function
+
+def encode(password):
+    encoded = ""
+    for num in password:
+        num = int(num)
+        if num >= 7:
+            encoded += str(num - 7)
+        else:
+            encoded += str(num + 3)
+    return encoded
+
+while True:
+    print()
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+    option = int(input("Please enter an option: "))
+
+
+    if option == 1:
+        password = input("Please enter your password to encode: ")
+        encoded_password = encode(password)
+        print(encoded_password)
+        print("Your password has been encoded and stored! ")
+    elif option == 2:
+        decoded_password = decode(encoded_password)
+        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}")
+    elif option == 3:
+        break
